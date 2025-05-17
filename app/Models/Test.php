4cf878bb14a434c20bb245b02f8cba3af11288fc
mahdiyar <smmhnp@gmail.com>
2025-05-17 10:52:53 +0330

    final commit from me

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -6,53 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Crypt;
 
 
-//....................................................Encrypt with middleware and Decrypt whit Model.........
-
-// class Test extends Model
-// {
-//     protected $table = 'users';
-    
-//     protected $fillable = [
-//         'firstname', 'lastname', 'nickname', 'email', 'password'
-//     ];
-
-//     protected $hidden = [
-//         'remember_token',
-//     ];
-
-
-//     protected function casts(): array
-//     {
-//         return [
-//             'email_verified_at' => 'datetime',
-//             'password' => 'hashed',
-//         ];
-//     }
-
-//     public function getFirstnameAttribute($value)
-//     {
-//         return Crypt::decryptString($value);
-//     }
-
-//     public function getLastnameAttribute($value)
-//     {
-//         return Crypt::decryptString($value);
-//     }
-
-//     public function getNicknameAttribute($value)
-//     {
-//         return Crypt::decryptString($value);
-//     }
-
-//     public function getEmailAttribute($value)
-//     {
-//         return Crypt::decryptString($value);
-//     }
-
-// }
-
-
-
 // ...................................................Encrypt & Decrypt whit model...........................
 
 class Test extends Model
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Middleware\EncryptUserData;
 
 Route::get('/', [TestController::class, 'create']) -> name('create');
 Route::post('/', [TestController::class, 'store']);
-// Route::post('/', [TestController::class, 'store']) -> middleware(EncryptUserData::class);
 
 
 Route::get('/show/{id}', [TestController::class, 'show']);
